4d44b0585339aa521b779e64ac39a0266475ce8b
Maiara Will <50561516+maiarawill@users.noreply.github.com>
2020-02-11 20:46:14 -0300

    Criar 10 botões (for)

---
+++ atividadeDois.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <meta charset = "utf-8"/>
+        <title> Curso php </title>
+        <style>
+        </style>
+    </head>
+
+    <body> 
+
+
+    <?php
+
+    $botao = 0;
+
+        for ($botao=0; $botao < 10 ; $botao++) { ?>
+
+           <button>oi</button>
+
+        <?php } ?>
+</body>
+
+</html>
+ 
---
+++ atividadeUm.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <meta charset = "utf-8"/>
+        <title> Curso php </title>
+        <style>
+        </style>
+    </head>
+
+    <body> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+
+    <?php
+
+    $botao = 0;
+
+        for ($botao=0; $botao < 10 ; $botao++) { ?>
+
+           <button>oi</button>
+
+        <?php } ?>
+
+ </body>
+
+</html>
